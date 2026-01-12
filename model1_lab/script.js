36334a493f7d1ed90c66ab744e2313f770559cc8
trevorwinger <54414318+trevorwinger@users.noreply.github.com>
2026-01-11 22:17:17 -0600

    Add files via upload

---
+++ model1_lab/script.js
@@ -0,0 +1,30 @@
+/*
+  CSC-204 Lab 1 JavaScript
+
+  TODO:
+  - Define the task array
+  - Track the current filter
+  - Write functions to:
+      * add a task
+      * toggle completion
+      * delete a task
+      * filter tasks
+      * render tasks
+  - Attach event listeners
+*/
+
+// TODO: Create an empty array to store tasks
+
+// TODO: Define a variable to track the active filter
+
+// TODO: Write a render() function that:
+// - clears the list container
+// - loops over visible tasks
+// - creates DOM elements for each task
+// - appends them to the page
+
+// TODO: Add event listeners for:
+// - adding a task
+// - toggling completion
+// - deleting tasks
+// - changing filters
